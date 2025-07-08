8cc5f26ca158db2db4a8bae8ee946d2a66456982
Paaxy <163489393+Paaxy@users.noreply.github.com>
2025-07-08 16:56:23 +0900

    Create menu.js

---
+++ scripts/menu.js
@@ -0,0 +1,19 @@
+function startGame() {
+  window.location.href = "game.html";
+}
+
+function openWorlds() {
+  window.location.href = "worldmenu.html";
+}
+
+function openMultiplayer() {
+  window.location.href = "multiplayer.html";
+}
+
+function openSettings() {
+  alert("Settings coming soon, ZIR!");
+}
+
+function openCredits() {
+  window.location.href = "poem.html";
+}
